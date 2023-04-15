02310bd366aeea8ec536c96f08313c2121158798
Dominik Koszkul <koszkuld@ryanair.com>
2023-04-15 15:42:13 +0200

    zad 1  - relacje

--- src/main/java/pl/coderslab/model/Publisher.java
+++ src/main/java/pl/coderslab/model/Publisher.java
@@ -8,7 +8,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -22,4 +25,8 @@ public class Publisher {
 
     @Column
     private String name;
+
+    // Dla przykładu relacja dwukierunkowa
+//    @OneToMany(mappedBy = "publisher")
+//    private Set<Book> books = new HashSet<>();
 }
